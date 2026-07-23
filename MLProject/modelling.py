95b2a05800ed3e3c659578fd0022fb894cc80906
JazeL2304 <163304081+JazeL2304@users.noreply.github.com>
2026-07-23 19:06:53 +0700

    Setup MLflow Project CI

---
+++ MLProject/modelling.py
@@ -0,0 +1,23 @@
+import mlflow
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+
+mlflow.set_experiment("Prediksi Risiko COVID Indonesia")
+
+# load data hasil preprocessing
+df = pd.read_csv("namadataset_preprocessing.csv")
+X = df.drop(columns=["target"])
+y = df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+mlflow.autolog()
+
+with mlflow.start_run(run_name="RandomForest_Basic"):
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    score = model.score(X_test, y_test)
+    print("Akurasi:", score)
